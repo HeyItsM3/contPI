f1935d8a4fe720eee48bb62bef164aff1290080f
Aldo Yedro <aldoyedro181@gmail.com>
2020-11-21 03:07:28 -0300

    V8 Alias & Login page

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,18 +1,24 @@
 // Dependencies
-
 import express from 'express'
 import next from 'next'
+import path from 'path'
 
 // Settings up Next App
-
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const handle = nextApp.getRequestHandler()
 
 // Running Next App
-
 nextApp.prepare().then(() => {
   const app = express()
+
+  // Public static
+  app.use(express.static(path.join(__dirname, '../public')))
+
+  // Routes
+  app.get('/login', (req: any, res: any) => {
+    return nextApp.render(req, res, '/users/login', req.query)
+  })
 
   app.all('*', (req: any, res: any) => {
     return handle(req, res)

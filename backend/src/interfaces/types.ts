3d26c753dfcde97cf297e019154b3a15955e3ff3
Aldo Yedro <aldoyedro181@gmail.com>
2020-12-04 02:05:50 -0300

    V15 Model, Field & Declaration

--- backend/src/interfaces/types.ts
+++ backend/src/interfaces/types.ts
@@ -5,6 +5,28 @@ export type App = {
   description: string
 }
 
+export type Declaration = {
+  declaration: string
+  icon: string
+  description: string
+}
+
+export type Field = {
+  type: string
+  fieldName: string
+  identifier: string
+  description: string
+  isRequired: boolean
+  isUnique: boolean
+  isHide: boolean
+}
+
+export type Model = {
+  modelName: string
+  identifier: string
+  description: string
+}
+
 export type User = {
   username: string
   password: string
@@ -12,6 +34,7 @@ export type User = {
   privilege: string
   active: boolean
 }
+
 export type Sequelize = {
   _defaults?: any
   name?: string
